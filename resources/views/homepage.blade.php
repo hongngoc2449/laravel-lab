be1a0bad4f9ebfa7e4c232d20ac0fe2ea250b51d
hongngocdoan <hongngoc2449@gmail.com>
2024-12-25 16:51:40 +0700

    create shoppping cart

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,4 +4,11 @@
     @foreach ($categories as $category)
         <x-product-grids :category="$category" :products="$category['products']" />
     @endforeach
+
+    <x-slot name="components">
+        <livewire:checkout.cart-bar />
+
+        <livewire:checkout.add-cart-item-popup />
+    </x-slot>
+
 </x-user-layout>
